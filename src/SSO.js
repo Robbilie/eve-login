f7638a0ddbc43e55cc7631fe51a794a429ea03ab
Robert Schuh <robert.schuh@valtech.de>
2018-03-20 20:18:27 +0100

    initial commit

---
+++ src/SSO.js
@@ -0,0 +1,102 @@
+"use strict";
+
+const qs = require("qs");
+
+class SSO {
+
+    static get LIVE_SERVER () {
+        return "https://login.eveonline.com";
+    }
+
+    static get TEST_SERVER1 () {
+        return "https://sisilogin.testeveonline.com";
+    }
+
+    static get TEST_SERVER2 () {
+        throw new Error("NotImplementedException");
+    }
+
+    static get TEST_SERVER3 () {
+        throw new Error("NotImplementedException");
+    }
+
+    static extractAccessToken (url) {
+        const length = url.indexOf(SSO.ACCESS_TOKEN_NEEDLE);
+        if (length === -1)
+            return null;
+        return url.substring(length + SSO.ACCESS_TOKEN_NEEDLE.length, url.indexOf("&"));
+    }
+
+    static requiresEULA (res) {
+        return res.match(new RegExp(SSO.EULA_NEEDLE, "i"));
+    }
+
+    static getEulaHash (res) {
+        const hashStart = res.match(new RegExp(SSO.EULA_HASH_NEEDLE, "i"));
+        if (!hashStart || hashStart.index === -1)
+            throw new Error("Missing EULA Hash!");
+        return res.substr(hashStart.index + SSO.EULA_HASH_NEEDLE.length, 32);
+    }
+
+    static requiresAuthenticator (res) {
+        return res.match(new RegExp(SSO.AUTHENTICATOR_NEEDLE, "i"));
+    }
+
+    static requiresCharacterName (res) {
+        return res.match(new RegExp(SSO.CHARACTER_NEEDLE, "g"));
+    }
+
+    static ACCOUNT_URL (baseUrl, accountUrl) {
+        return `${baseUrl}${accountUrl}?${qs.stringify({
+            ReturnUrl: SSO.RETURN_URL(baseUrl),
+        })}`;
+    }
+
+    static LOGIN_REFERER_URL (baseUrl) {
+        return SSO.ACCOUNT_URL(baseUrl, "/Account/LogOn");
+    }
+
+    static RETURN_URL (baseUrl) {
+        return `/oauth/authorize/?${qs.stringify({
+            client_id: "eveLauncherTQ",
+            lang: "en",
+            response_type: "token",
+            redirect_uri: `${baseUrl}/launcher?${qs.stringify({
+                client_id: "eveLauncherTQ",
+                scope: "eveClientToken"
+            })}`,
+        }, { encode: false })}`;
+    }
+
+    static CONFIRM_CHARACTER_URL (baseUrl) {
+        return SSO.ACCOUNT_URL(baseUrl, "/Account/Challenge");
+    }
+
+    static CONFIRM_AUTHENTICATOR_URL (baseUrl) {
+        return SSO.ACCOUNT_URL(baseUrl, "/Account/Authenticator");
+    }
+
+    static CONFIRM_EULA_URL (baseUrl) {
+        return `${baseUrl}/OAuth/Eula`;
+    }
+
+    static CONFIRM_EULA_RETURN_URL (baseUrl) {
+        return SSO.RETURN_URL(baseUrl);
+    }
+
+    static ACCESS_TOKEN_URL (baseUrl, accessToken) {
+        return `${baseUrl}/launcher/token?accesstoken=${accessToken}`;
+    }
+
+}
+
+SSO.FRAGMENT_WARNING = `No Fragment found. 
+Make sure your login details are correct and your system clock is correct when using authenticator. 
+If you believe it's not your fault please report this issue.`;
+SSO.CHARACTER_NEEDLE = "please enter the name of one of the characters associated with your account";
+SSO.AUTHENTICATOR_NEEDLE = "action=\"/Account/Authenticator";
+SSO.EULA_NEEDLE = "action=\"/oauth/eula\"";
+SSO.EULA_HASH_NEEDLE = "name=\"eulaHash\" type=\"hidden\" value=\"";
+SSO.ACCESS_TOKEN_NEEDLE = "#access_token=";
+
+module.exports = SSO;
---
+++ src/SSOConnection.js
@@ -0,0 +1,122 @@
+"use strict";
+
+const rrr = require("request-promise-native");
+const speakeasy = require("speakeasy");
+
+const { SSO } = require(".");
+
+class SSOConnection {
+
+    constructor (baseUrl = SSO.LIVE_SERVER, args = {}) {
+        this.request = rrr.defaults({
+            jar: rrr.jar(),
+            simple: false,
+            resolveWithFullResponse: true,
+            //followRedirect: false,
+            followAllRedirects: true,
+            ...args,
+        });
+        this.baseUrl = baseUrl;
+    }
+
+    async getLoginToken (args) {
+        const accessToken = await this.getAccessToken(args);
+        if (!accessToken)
+            return null;
+        return this.exchangeAccessToken(accessToken);
+    }
+
+    async exchangeAccessToken (accessToken) {
+        const uri = SSO.ACCESS_TOKEN_URL(this.baseUrl, accessToken);
+        const response = await this.request({
+            method: "GET",
+            uri,
+        });
+        return SSO.extractAccessToken(response.request.uri.href);
+    }
+
+    async getAccessToken ({ username, password, characterName, secret }) {
+        let [access_token, res] = await this.handleInitialRequest(username, password);
+        if (!access_token && SSO.requiresCharacterName(res))
+            [access_token, res] = await this.handleRequiresCharacterName(characterName);
+        if (!access_token && SSO.requiresAuthenticator(res))
+            [access_token, res] = await this.handleRequiresAuthenticator(secret);
+        if (!access_token && SSO.requiresEULA(res))
+            [access_token, res] = await this.handleRequiresEULA(res);
+        if (!access_token)
+            throw SSO.FRAGMENT_WARNING;
+        return access_token;
+    }
+
+    async handleInitialRequest (UserName, Password) {
+        const response = await this.request({
+            method: "POST",
+            uri: SSO.LOGIN_REFERER_URL(this.baseUrl),
+            form: {
+                UserName,
+                Password,
+            }
+        });
+
+        return [SSO.extractAccessToken(response.request.uri.href), response.body];
+    }
+
+    async handleRequiresCharacterName (characterName) {
+        console.log("characterName is required!");
+        if (!characterName)
+            throw new Error("No CharacterName set! Aborting...");
+
+        const response = await this.request({
+            method: "POST",
+            uri: SSO.CONFIRM_CHARACTER_URL(this.baseUrl),
+            form: {
+                Challenge: characterName,
+                RememberCharacterChallenge: true,
+            }
+        });
+
+        return [SSO.extractAccessToken(response.request.uri.href), response.body];
+    }
+
+    async handleRequiresAuthenticator (secret) {
+        console.log("Authenticator required!");
+        if (!secret)
+            throw new Error("No 2FA secret set! Aborting...");
+
+        const pin = speakeasy.totp({ secret, encoding: "base32" });
+        console.log(`Using pin ${pin}`);
+
+        const response = await this.request({
+            method: "POST",
+            uri: SSO.CONFIRM_AUTHENTICATOR_URL(this.baseUrl),
+            form: {
+                Challenge: pin,
+                RememberTwoFactor: true,
+                command: "Continue",
+            }
+        });
+
+        return [SSO.extractAccessToken(response.request.uri.href), response.body];
+    }
+
+    async handleRequiresEULA (res) {
+        console.log("Eula required!");
+        const eulaHash = SSO.getEulaHash(res);
+        console.log(`Eula hash: ${eulaHash}`);
+
+        const response = await this.request({
+            method: "POST",
+            uri: SSO.CONFIRM_EULA_URL(this.baseUrl),
+            form: {
+                eulaHash,
+                returnUrl: SSO.CONFIRM_EULA_RETURN_URL(this.baseUrl),
+                action: "Accept",
+            }
+        });
+
+        return [SSO.extractAccessToken(response.request.uri.href), response.body];
+    }
+
+}
+
+module.exports = SSOConnection;
